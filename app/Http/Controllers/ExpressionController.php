27c55ac28dd112f279019ebff2c37b9bcbd7ae5f
Jeferson Oliveira <zuruckyt@gmail.com>
2020-10-02 00:31:02 -0300

    Add expression controller methods

--- app/Http/Controllers/ExpressionController.php
+++ app/Http/Controllers/ExpressionController.php
@@ -2,9 +2,51 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreExpression;
+use App\Http\Requests\UpdateExpression;
+use App\Models\Expression;
+use App\Models\Tag;
 use Illuminate\Http\Request;
 
 class ExpressionController extends Controller
 {
-    //
+    public function create()
+    {
+        return view('expressions.create', [
+            'tags' => Tag::all(),
+        ]);
+    }
+
+    public function delete(Expression $expression){
+        //TODO
+    }
+    public function edit(Expression $expression)
+    {
+        return view('expressions.create', [
+            'expression' => $expression,
+            'tags' => Tag::all(),
+        ]);
+    }
+
+    public function index() {
+        return view('expressions.all'. [
+            'expressions' => Expression::all()
+        ]);
+    }
+
+    public function store(StoreExpression $request)
+    {   
+        //TODO
+    }
+
+    public function show(Expression $expression)
+    {
+        return view('expression.single', [
+            'expression' => $expression
+        ]);
+    }
+
+    public function update(UpdateExpression $request) {
+        //todo
+    }
 }
