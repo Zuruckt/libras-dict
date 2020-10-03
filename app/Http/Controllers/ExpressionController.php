04a742188a3adce5e4950b3a9da35e058232b105
Jeferson Oliveira <zuruckyt@gmail.com>
2020-10-02 21:35:26 -0300

    Add create expression logic

--- app/Http/Controllers/ExpressionController.php
+++ app/Http/Controllers/ExpressionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\FileHelper;
 use App\Http\Requests\StoreExpression;
 use App\Http\Requests\UpdateExpression;
 use App\Models\Expression;
@@ -9,7 +10,13 @@ use App\Models\Tag;
 use Illuminate\Http\Request;
 
 class ExpressionController extends Controller
-{
+{   
+    private $helper;
+
+    public function __construct()
+    {
+        $this->helper = new FileHelper;
+    }
     public function create()
     {
         return view('expressions.create', [
@@ -29,14 +36,35 @@ class ExpressionController extends Controller
     }
 
     public function index() {
-        return view('expressions.all'. [
-            'expressions' => Expression::all()
+        return view('expressions.all', [
+            'expressions' => Expression::all(),
+            'tags' => Tag::all()
         ]);
     }
 
     public function store(StoreExpression $request)
     {   
-        //TODO
+        $request->validate($request->rules());
+        
+        $data = array_merge(
+            $request->except('tags'),
+            [
+                'user_id' => auth()->id(),
+                'gif_path' => $this->helper->store($request->file, 'public')
+            ]
+        );
+
+        $expression = Expression::create($data);
+
+        if (!$expression) {
+            abort(500, 'Não foi possível salvar a expressão.');
+        }
+
+        if (!$expression->tags()->sync($request->tags)) {
+            abort(500, 'Não foi possível sincronizar as Tags.');
+        }
+
+        return redirect('/expression')->with('expression_created', 'Expressão registrada com sucesso');
     }
 
     public function show(Expression $expression)
